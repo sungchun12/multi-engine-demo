4948f3731ecb848b41d3a027a50c11b2e467a730
Sung Won Chung <sungwonchung3@gmail.com>
2025-01-08 15:53:39 -0800

    working example

--- models/duckdb_model.sql
+++ models/duckdb_model.sql
@@ -1,13 +1,21 @@
 MODEL (
   name sqlmesh_example.duckdb_model,
-  kind FULL,
+  kind INCREMENTAL_BY_TIME_RANGE (
+    time_column event_date
+  ),
+  start '2020-01-01',
+  cron '@daily',
   gateway duckdb,
+  dialect duckdb,
 );
 
+-- this will read from postgres, process data in duckdb, and write it back to postgres
 SELECT
-    reviewid,
-    username,
-    review,
-    ingestion_date
-FROM 
-    iceberg_scan('s3://multi-engine-demo-bucket/landing_reviews/metadata/00000-19d8f4a6-f7c9-47bf-bc29-3041607f8b90.metadata.json', allow_moved_paths = true);
+  id,
+  item_id,
+  event_date,
+FROM
+  sqlmesh_example.seed_model -- let's imagine this is a large 1 billion row table
+WHERE
+  event_date BETWEEN @start_date AND @end_date
+  
